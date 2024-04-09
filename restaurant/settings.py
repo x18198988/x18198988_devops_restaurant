430cb9b3c1e780ba69c49bc5e47747461533b268
x18198988 <159906039+x18198988@users.noreply.github.com>
2024-04-09 23:43:49 +0530

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
