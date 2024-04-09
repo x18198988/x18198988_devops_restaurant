9a414be4249ba6bbd2ebd19d5674f71d090c9fd7
x18198988 <159906039+x18198988@users.noreply.github.com>
2024-04-09 09:41:51 +0100

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,3 +3,5 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/restaurant/requirements.txt
+sudo chmod 777 /home/ubuntu/restaurant
+sudo chmod 777 /home/ubuntu/restaurant/db.sqlite3
