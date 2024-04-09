c78c7e53f97cb2d1055ff3708c0585f289bcefb2
x18198988 <159906039+x18198988@users.noreply.github.com>
2024-04-09 23:40:54 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,9 +2,9 @@
 
 sed -i 's/\[]/\["18.201.240.183"]/' /home/ubuntu/restaurant/restaurant/settings.py
 
-python3 manage.py migrate 
-python3 manage.py makemigrations     
-python3 /home/ubuntu/restaurant/manage.py collectstatic --noinput
+python manage.py migrate 
+python manage.py makemigrations     
+python manage.py collectstatic --noinput
 sudo service gunicorn restart
 sudo service nginx restart
 #sudo tail -f /var/log/nginx/error.log
